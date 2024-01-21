9d64031ea7f4b87c9e0942a74d952757d34b0c5e
Tomara Petty <tomarapetty@Tomaras-MacBook-Air.local>
2024-01-21 14:39:20 -0800

    adding accidental deleted files

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,23 @@ import React, { Fragment } from 'react';
 import Navbar from './layout/Navbar';
 import Landing from './layout/Landing';
 import './App.css';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 
 const App = () => (
   <Fragment>
-    <Navbar />
-    <Landing />
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Landing />} />
+        <Route path='register' element={<Register />} />
+        <Route path='login' element={<Login />} />
+      </Routes>
+    </Router>
   </Fragment>
 );
 
