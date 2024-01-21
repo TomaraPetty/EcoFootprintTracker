d7adfc82c25d1cf7574fa04ee527c0c21f84b53b
Tomara Petty <tomarapetty@Tomaras-MacBook-Air.local>
2024-01-21 14:57:13 -0800

    fix issues with build preventing app from starting up

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,7 @@ import React, { Fragment } from 'react';
 import Navbar from './layout/Navbar';
 import Landing from './layout/Landing';
 import './App.css';
-import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './components/layout/Navbar';
-import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 
