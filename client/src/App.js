94b644c394b9ef0a178e161db76c9210e007adf1
Tomara Petty <tomarapetty@Tomaras-MacBook-Air.local>
2024-01-22 18:13:39 -0800

    add alert for wrong password

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React, { Fragment } from 'react';
-import Navbar from './layout/Navbar';
-import Landing from './layout/Landing';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Alert from './components/layout/Alert';
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
@@ -14,11 +15,14 @@ const App = () => (
     <Router>
       <Fragment>
         <Navbar />
-        <Routes>
-          <Route path='register' element={<Register />} />
-          <Route path='login' element={<Login />} />
-          <Route path='/' element={<Landing />} />
-        </Routes>
+        <section className='container'>
+          <Alert />
+          <Routes>
+            <Route path='register' element={<Register />} />
+            <Route path='login' element={<Login />} />
+            <Route path='/' element={<Landing />} />
+          </Routes>
+        </section>
       </Fragment>
     </Router>
   </Provider>
