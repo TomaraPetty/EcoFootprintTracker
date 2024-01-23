aa635dd2c7a8e4e5806faa45cb153bdea6de9bf6
Tomara Petty <tomarapetty@Tomaras-MacBook-Air.local>
2024-01-22 19:22:03 -0800

    add setAuthToken util

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,6 +1,14 @@
 import axios from 'axios';
 import { setAlert } from './alert';
-import { REGISTER_SUCCESS, REGISTER_FAIL } from './types';
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+} from './types';
+
+// load user
+export const loadUser = () => async (dispatch) => {};
 
 // register user
 export const register =
