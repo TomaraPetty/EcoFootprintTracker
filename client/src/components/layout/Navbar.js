a41cc5c1eba3755ed1446a03ba144f256afb1ef9
Tomara Petty <tomarapetty@Tomaras-MacBook-Air.local>
2024-02-02 16:54:08 -0800

    change icon for logo

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -40,7 +40,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
     <nav className='navbar bg-dark'>
       <h1>
         <Link to='/'>
-          <i className='fas fa-code'></i> EcoFootprint Tracker
+          <i className='fas fa-leaf'></i> EcoFootprint Tracker
         </Link>
       </h1>
       {!loading && (
