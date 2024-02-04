f5788ae19e69ca84f7df1ed2acc512d1aca08986
Tomara Petty <tomarapetty@Tomaras-MacBook-Air.local>
2024-02-03 17:52:03 -0800

    create profile about section and add to profile component

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -4,7 +4,7 @@ import { Link, useParams } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import ProfileTop from './ProfileTop';
-// import ProfileAbout from './ProfileAbout';
+import ProfileAbout from './ProfileAbout';
 // import ProfileExperience from './ProfileExperience';
 // import ProfileEducation from './ProfileEducation';
 // import ProfileGithub from './ProfileGithub';
@@ -34,7 +34,7 @@ const Profile = ({ getProfileById, profile: { profile }, auth }) => {
             )}
           <div className='profile-grid my-1'>
             <ProfileTop profile={profile} />
-            {/* <ProfileAbout profile={profile} /> */}
+            <ProfileAbout profile={profile} />
             {/* <div className='profile-exp bg-white p-2'>
               <h2 className='text-primary'>Experience</h2>
               {profile.experience.length > 0 ? (
