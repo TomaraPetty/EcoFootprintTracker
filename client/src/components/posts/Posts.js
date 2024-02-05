84f2467534802e26777daab385620be72ba9b246
Tomara Petty <tomarapetty@Tomaras-MacBook-Air.local>
2024-02-04 17:56:51 -0800

    add posts component

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -1,14 +1,29 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { getPosts } from '../../actions/post';
 import { connect } from 'react-redux';
+import PostItem from './PostItem';
+import PostForm from './PostForm';
+import { getPosts } from '../../actions/post';
 
-const Posts = ({ getPosts, post: { posts, loading } }) => {
+const Posts = ({ getPosts, post: { posts } }) => {
   useEffect(() => {
     getPosts();
   }, [getPosts]);
 
-  return <></>;
+  return (
+    <section className='container'>
+      <h1 className='large text-primary'>Posts</h1>
+      <p className='lead'>
+        <i className='fas fa-user' /> Welcome to the community
+      </p>
+      <PostForm />
+      <div className='posts'>
+        {posts.map((post) => (
+          <PostItem key={post._id} post={post} />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 Posts.propTypes = {
